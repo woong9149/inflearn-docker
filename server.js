9997078f1ea2a73d1d8685ba3f25fa8838f98ec4
woong9149 <sewoong0409@naver.com>
2023-04-21 19:32:53 +0900

    230421 create docker file 230421 create docker file 230421 create docker file 230421 create docker file 230421 create docker file 230421 create docker file 230421 create docker file 230421 create docker file 230421 create docker file 230421 create docker file 230421 create docker file 230421 create docker file 230421 create docker file 230421 create docker file 230421 create docker file 230421 create docker file 230421 create docker file 230421 create docker file 230421 create docker file 230421 create docker file

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const PORT = 8080;
 //APP
 const app = express();
 app.get('/', (req,res) => {
-  res.send("Hello World");
+  res.send("Hello World !!!!!!");
 });
 
 app.listen(PORT);
+
+console.log("server is running");
